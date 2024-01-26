a624ff283d98df259e7937ab9642bfddaf542e2f
BrandonWhoKnows <br.serafim@gmail.com>
2024-01-26 01:55:50 -0300

    primeiro commit

---
+++ index2.js
@@ -0,0 +1,24 @@
+let wins = 99
+let losses = 22
+
+let matchProfit = wins - losses
+
+getHeroRank(matchProfit)
+
+function getHeroRank(matchProfit){
+    let heroRank;
+        if (matchProfit <= 10){
+            heroRank = "Ferro"}
+        else if (matchProfit >= 11 && matchProfit <= 20){
+            heroRank = "Bronze"}
+        else if (matchProfit >= 21 && matchProfit <= 50){
+            heroRank = "Prata"}
+        else if (matchProfit >= 51 && matchProfit <= 80){
+            heroRank = "Ouro"}
+        else if (matchProfit >= 81 && matchProfit <= 90){
+            heroRank = "Diamante"}
+        else if (matchProfit >= 91 && matchProfit <= 100){
+            heroRank = "Lendário"}
+        else {heroRank = "Imortal"}
+    console.log(`O herói tem saldo de ${matchProfit} e está no nível ${heroRank}`)
+}
